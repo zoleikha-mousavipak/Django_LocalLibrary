05bb367c5ff9f751a930227c337deba26b79ac6e
zoleikha-mousavipak <zmpak2000@gmail.com>
2019-02-09 23:00:16 +0330

    201909002 locallibSite-testing

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from . import views
 
-urlpatterns =[
+app_name='catalog'
 
-
+urlpatterns = [
+    path('', views.Index.as_view() , name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
 ]
